14ebb6b23385edfdfa729e50358026f3f9a2024f
Сергей Квин <lorancem@mail.ru>
2022-09-27 22:27:48 +0300

    Lesson 3

---
+++ src/test/java/com/demoqa/JUnitDemoTest.java
@@ -0,0 +1,32 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+public class JUnitDemoTest {
+    @BeforeAll
+    static void  setUp() {
+        System.out.println("### @BeforAll !");
+    }
+    @AfterAll
+    static void  tearDown() {
+        System.out.println("### @AfterAll !");
+    }
+    @BeforeEach
+        void beforeEachTest(){
+        System.out.println("### @BeforeEach !");
+    }
+    @AfterEach
+    void AfterEachTest(){
+        System.out.println("### @AfterEach !");
+    }
+    @Test
+    void simpleTestFirst() {
+        System.out.println("###   @Test ssimpleTestFirst !");
+        Assertions.assertTrue(3>2);
+    }
+    @Test
+    void simpleTestSecond() {
+        System.out.println("###   @Test simpleTestSecond !");
+        Assertions.assertTrue(3>2);
+    }
+}
